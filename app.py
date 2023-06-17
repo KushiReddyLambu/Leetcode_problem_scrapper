f3830f926f1a84c2bafe9c7152a16cecd182da16
KushiReddyLambu <101383839+KushiReddyLambu@users.noreply.github.com>
2023-06-17 16:27:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -20,9 +20,9 @@ def problem_title(num):
 
 def load_vocab():
     vocab = {}
-    with open('vocab.txt', 'r') as f:
+    with open('vocab.txt', 'r',encoding='utf-8') as f:
         vocab_terms = f.readlines()
-    with open('idf-values.txt', 'r') as f:
+    with open('idf-values.txt', 'r',encoding='utf-8') as f:
         idf_values = f.readlines()
 
     for (term, idf_value) in zip(vocab_terms, idf_values):
@@ -33,7 +33,7 @@ def load_vocab():
 
 def load_documents():
     documents = []
-    with open('documents.txt', 'r') as f:
+    with open('documents.txt', 'r',encoding='utf-8') as f:
         documents = f.readlines()
     documents = [document.strip().split() for document in documents]
 
@@ -44,7 +44,7 @@ def load_documents():
 
 def load_inverted_index():
     inverted_index = {}
-    with open('inverted-index.txt', 'r') as f:
+    with open('inverted-index.txt', 'r',encoding='utf-8') as f:
         inverted_index_terms = f.readlines()
 
     for row_num in range(0, len(inverted_index_terms), 2):
