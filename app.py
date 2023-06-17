75105bd888da6c23c562f72248e7fc56d597d93b
KushiReddyLambu <101383839+KushiReddyLambu@users.noreply.github.com>
2023-06-17 18:00:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,22 +7,22 @@ from wtforms import StringField, SubmitField
 
 
 def problem_link(num):
-    with open('Qdata/Qindex.txt', 'r') as f:
+    with open('Qdata/Qindex.txt', 'r', encoding='ISO-8859-1') as f:
         links = f.readlines()
     return links[num]
 
 
 def problem_title(num):
-    with open('Qdata/index.txt', 'r') as f:
+    with open('Qdata/index.txt', 'r', encoding='ISO-8859-1') as f:
         headings = f.readlines()
     return str(headings[num].split(' ', 1)[1])
 
 
 def load_vocab():
     vocab = {}
-    with open('vocab.txt', 'r',encoding='utf-8') as f:
+    with open('vocab.txt', 'r', encoding='ISO-8859-1') as f:
         vocab_terms = f.readlines()
-    with open('idf-values.txt', 'r',encoding='utf-8') as f:
+    with open('idf-values.txt', 'r', encoding='ISO-8859-1') as f:
         idf_values = f.readlines()
 
     for (term, idf_value) in zip(vocab_terms, idf_values):
@@ -33,7 +33,7 @@ def load_vocab():
 
 def load_documents():
     documents = []
-    with open('documents.txt', 'r',encoding='utf-8') as f:
+    with open('documents.txt', 'r', encoding='ISO-8859-1') as f:
         documents = f.readlines()
     documents = [document.strip().split() for document in documents]
 
@@ -44,7 +44,7 @@ def load_documents():
 
 def load_inverted_index():
     inverted_index = {}
-    with open('inverted-index.txt', 'r',encoding='utf-8') as f:
+    with open('inverted-index.txt', 'r', encoding='ISO-8859-1') as f:
         inverted_index_terms = f.readlines()
 
     for row_num in range(0, len(inverted_index_terms), 2):
